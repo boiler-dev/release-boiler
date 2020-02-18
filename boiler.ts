49adb4ae214831842c6c13818f0e376dc683eec8
winton <mail@wintoni.us>
2020-02-17 22:51:44 -0800

    Private project prompt

--- boiler.ts
+++ boiler.ts
@@ -1,14 +1,37 @@
 import { join } from "path"
-import { InstallBoiler } from "boiler-dev"
+import { InstallBoiler, PromptBoiler } from "boiler-dev"
+
+export const promptBoiler: PromptBoiler = async () => {
+  return [
+    {
+      type: "confirm",
+      name: "private",
+      message: "private project?",
+      default: false,
+    },
+  ]
+}
 
 export const installBoiler: InstallBoiler = async ({
+  answers,
   destDir,
   files,
 }) => {
   const actions = []
 
-  for (const { name, source } of files) {
+  for (const file of files) {
+    const { name } = file
+
     if (name === "release") {
+      let { source } = file
+
+      if (answers.private) {
+        source = source.replace(
+          /--ci/gm,
+          "--ci --no-npm.publish"
+        )
+      }
+
       actions.push({
         action: "write",
         bin: true,
@@ -23,6 +46,14 @@ export const installBoiler: InstallBoiler = async ({
     dev: true,
     source: ["release-it", "semver"],
   })
+
+  if (answers.private) {
+    actions.push({
+      action: "merge",
+      path: join(destDir, "package.json"),
+      source: { private: true },
+    })
+  }
 
   return actions
 }
